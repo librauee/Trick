d8015e7c2975164d52648efa2d77e099cb15feb5
librauee <870407139@qq.com>
2019-09-05 20:53:16 +0800

    520照片墙

---
+++ 520picture/pic_rename.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 20:06:31 2019
+
+@author: Lee
+
+图片批量重命名
+"""
+
+
+
+import os
+ 
+class ImageRename():
+     def __init__(self):
+         self.path = 'D:/BaiduNetdiskDownload/肥肥'
+ 
+     def rename(self):
+         filelist = os.listdir(self.path)
+         total_num = len(filelist) 
+         i = 0
+         for item in filelist:
+             if item.endswith('.jpg'):
+                 src = os.path.join(os.path.abspath(self.path), item)
+                 dst = os.path.join(os.path.abspath(self.path),  str(i) + '.jpg')
+                 os.rename(src, dst)
+                 print('converting %s to %s ...' % (src, dst))
+             i+=1
+         print('total %d to rename & converted %d jpgs' % (total_num, i))
+         
+         
+class ImageRename2():  
+    def __init__(self):
+         self.path = '肥肥'   
+    
+    def rename(self):
+        for idx, each in enumerate(os.listdir(self.path)):
+	        os.rename(os.path.join(self.path, each), os.path.join(self.path, '%s.jpg' % idx))
+ 
+if __name__ == '__main__':
+    newname = ImageRename2()
+    newname.rename()
---
+++ 520picture/wallPictures.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 09:55:30 2019
+
+@author: Lee
+"""
+import os
+import argparse
+from PIL import Image
+
+
+CELLSIZE = 64
+
+'''图片读取'''
+def readImage(img_path, target_size=(64, 64)):
+	img = Image.open(img_path)
+	img = img.resize(target_size)
+	return img
+
+
+'''图片生成器'''
+def yieldImage(target_dir, idx, target_size):
+	img_paths = sorted([os.path.join(target_dir, imgname) for imgname in os.listdir(target_dir)])
+	idx = (idx + 1) % len(img_paths)
+	return readImage(img_paths[idx], target_size), idx
+
+
+'''解析模板'''
+def parseTemplate(template_path):
+	template = []
+	with open(template_path, 'r') as f:
+		for line in f.readlines():
+			if line.startswith('#'):
+				continue
+			template.append(line.strip('\n').split(','))
+	return template
+
+
+'''主函数'''
+def main(pictures_dir, template_path):
+	template = parseTemplate(template_path)
+
+	w = len(template[0])
+	h = len(template)
+	image_new = Image.new('RGBA', (CELLSIZE*w, CELLSIZE*h))
+	img_idx = -1
+	for y in range(h):
+		for x in range(w):
+			if template[y][x] == '1':
+				img, img_idx = yieldImage(pictures_dir, img_idx, (CELLSIZE, CELLSIZE))
+				image_new.paste(img, (x*CELLSIZE, y*CELLSIZE))
+	image_new.show()
+	image_new.save('picturewall.png')
+
+
+'''run'''
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="Picture Wall Generator.")
+	parser.add_argument('-t', dest='template_path', help='Template path.', default='templates/2.tmp')
+	parser.add_argument('-p', dest='pictures_dir', help='Pictures dir.', default='肥肥')
+	args = parser.parse_args()
+	template_path = args.template_path
+	pictures_dir = args.pictures_dir
+	main(pictures_dir, template_path)
