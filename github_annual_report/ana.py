54f07766a94e0208e1b033d50b8030a7092816d1
librauee <870407139@qq.com>
2020-01-01 11:48:26 +0800

    年度报告分析

--- github_annual_report/ana.py
+++ github_annual_report/ana.py
@@ -7,12 +7,16 @@ Created on Mon Dec 30 14:42:44 2019
 
 from pymongo import MongoClient
 import pandas as pd
+import time
+import datetime
 
 
 
 """
 数据预处理
 """
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 db=MongoClient().github
 data=pd.DataFrame(list(db['day'].find()))
 data=data.drop(columns='_id',axis=0)
@@ -53,3 +57,23 @@ count=list(month_repo['count'].values)
 
 print(sum(count))
 
+
+"""
+代码提交最晚时间
+"""
+
+def rectify(t):
+    """
+    时间校正,北京在东八区 +8小时
+    """
+    temp=t.replace("T"," ").replace("Z","")
+    temp=datetime.datetime.strptime(temp,'%Y-%m-%d %H:%M:%S')
+    new_t=temp+datetime.timedelta(hours=16)
+    # mktime化为标准时间
+    total_second=time.mktime(new_t.timetuple())
+    second=total_second%86400
+    return second
+
+data2=pd.DataFrame(list(db['time'].find()))
+data2['commit_time']=data2['commit_time'].apply(rectify)
+print(data2.sort_values(by="commit_time")[:100])
